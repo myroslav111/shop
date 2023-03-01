95867285795599c77e666a5bfa976084be489fba
VolodymyrKavun <92682811+VolodymyrKavun@users.noreply.github.com>
2023-03-01 20:21:52 +0200

    add_aboutUs_and_accordion

---
+++ app/components/data/text-info/text-info.data.ts
@@ -0,0 +1,41 @@
+import {
+  IInfoText,
+  ITextContentOnAboutPage
+} from '../../../types/text-info.interface';
+
+export const textInfoAboutUs: IInfoText = {
+  primary:
+    'Essen und Trinken in reichhaltiger Auswahl: Das Starbucks Angebot an Getränken und Snacks Nur wenig ist für unsere Lebensqualität. ',
+  secondary:
+    'Gönne dir eine perfekte Tasse Kaffee und ein paar ausgesuchte, leckere Snacks - zu jedem Zeitpunkt.'
+};
+
+export const textInfoForMenu: IInfoText = {
+  primary:
+    'Essen und Trinken in reichhaltiger Auswahl: Das Starbucks Angebot an Getränken und Snacks Nur wenig ist für unsere Lebensqu',
+  secondary:
+    'alität so wichtig wie Essen und Trinken. Und bei unserer Starbucks Auswahlkannst du dir sicher sein, dass alles, was du dir aussuchst, vonbester Qualität ist. Gönne dir eine perfekte Tasse Kaffee und einpaar ausgesuchte, leckere Snacks - zu jedem Zeitpunkt.'
+};
+
+export const textContentOnAboutPage: ITextContentOnAboutPage[] = [
+  {
+    title: 'About Us',
+    paragraph:
+      'It happens millions of times each week – a customer receives a drink from a Starbucks barista – but each interaction is unique.'
+  },
+  {
+    title: 'Our Heritage',
+    paragraph:
+      'Every day, we go to work hoping to do two things: share great coffee with our friends and help make the world a little better.'
+  },
+  {
+    title: 'Ours Mission',
+    paragraph:
+      'To inspire and nurture the human spirit – one person, one cup and one neighbourhood at a time.'
+  },
+  {
+    title: 'Ours Values',
+    paragraph:
+      'Creating a culture of warmth and belonging, where everyone is welcome.'
+  }
+];
--- app/components/screens/menu/MenuPage.tsx
+++ app/components/screens/menu/MenuPage.tsx
@@ -1,10 +1,12 @@
 import { FC } from 'react';
 
 import { card } from '@/components/data/card.data';
+import { textInfoForMenu } from '@/components/data/text-info/text-info.data';
 import Card from '@/components/ui/card/Card';
-import Heading from '@/components/ui/heading/Heading';
 
 import Layout from '@/layout/meta/Layout';
+
+import AccordionText from '../../ui/accordion/AccordionText';
 
 const MenuPage: FC = () => {
   return (
@@ -15,15 +17,12 @@ const MenuPage: FC = () => {
         </div>
         <div className='mx-auto px-4'>
           <h1 className='text-2xl text-black font-bold mb-4'>Unser Angebot</h1>
-          <p className='max-w-2xl text-darck-beige mb-10'>
-            Essen und Trinken in reichhaltiger Auswahl: Das Starbucks Angebot an
-            Getränken und Snacks Nur wenig ist für unsere Lebensqualität so
-            wichtig wie Essen und Trinken. Und bei unserer Starbucks Auswahl
-            kannst du dir sicher sein, dass alles, was du dir aussuchst, von
-            bester Qualität ist. Gönne dir eine perfekte Tasse Kaffee und ein
-            paar ausgesuchte, leckere Snacks - zu jedem Zeitpunkt.
-          </p>
-          <div className='mx-auto lg:grid lg:grid-cols-2 lg:gap-6 flex flex-col gap-4'>
+          <AccordionText
+            primary={textInfoForMenu.primary}
+            secondary={textInfoForMenu.secondary}
+            className={'max-w-2xl text-darck-beige'}
+          />
+          <div className='mx-auto lg:grid lg:grid-cols-2 lg:gap-6 flex flex-col gap-4 mt-10'>
             {card.map(cardItem => (
               <div className='place-content-center flex'>
                 <Card
