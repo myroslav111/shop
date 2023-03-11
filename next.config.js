5450ec7bdb2c67091391b681c437901d70bd7e50
myroslav <myrkozar@gmail.com>
2023-03-11 16:28:34 +0200

    add next config for image

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+        port: '',
+        pathname: '**'
+      }
+    ]
+  },
   experimental: {
     esmExternals: false
   },
