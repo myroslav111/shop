31e5d1f833cc1e1c5649113ea05028ee5c1e1790
myroslav <myrkozar@gmail.com>
2023-07-08 20:15:40 +0300

    add registration

---
+++ app/components/screens/login-page/LoginPage.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react';
+
+import Heading from '@/components/ui/heading/Heading';
+
+import Layout from '@/layout/meta/Layout';
+
+const LoginPage: FC = () => {
+  return (
+    <Layout title='LoginPage' description='LoginPage'>
+      <Heading>Login</Heading>
+    </Layout>
+  );
+};
+
+export default LoginPage;
---
+++ pages/login/Login.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+import LoginPage from '@/components/screens/login-page/LoginPage';
+
+const Login: FC = () => {
+  return <LoginPage />;
+};
+
+export default Login;
